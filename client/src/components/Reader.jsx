c1b9f521896e5d14eb62309292dabbd364dfcf82
Alexius Calamus <AlexiusCalamus@users.noreply.github.com>
2016-05-09 01:17:28 +0200

    Add Fullscreen and Zoom to reader

--- client/src/components/Reader.jsx
+++ client/src/components/Reader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-class Reader extends React.Component {
+class Reader extends React.Component { 
+       
      constructor(props) {
          super(props);         
          this.diplayedTiles = 5;
@@ -9,11 +10,37 @@ class Reader extends React.Component {
              previous : "this.props.previous",
              next : "this.props.next",
              current : "http://i5.mangareader.net/one-piece/1/one-piece-1668161.jpg", //this.props.current
-             isBright : true
+             isBright : true,
+             isFullscreen : false
          };
          this.navNext.bind(this);
          this.navPrevious.bind(this);
          this.toggleFade.bind(this);
+         this.toggleFullscreen.bind(this);
+         this.zoomIn.bind(this);
+         this.zoomOut.bind(this);
+         this.zoomOff.bind(this);
+         this.fullScreenIsClosing.bind(this);
+     }  
+     
+     componentDidMount () {     
+          // document.getElementById("readerImage").addEventListener('load', function() {
+          //   console.log('My width is: ', this.naturalWidth);
+          //   console.log('My height is: ', this.naturalHeight);
+          //   document.getElementById("readerContent").height = this.naturalHeight;
+          // });
+          console.log(document.getElementById("readerContent").style.height);
+          document.addEventListener("fullscreenchange",() => this.fullScreenIsClosing(), false);
+          document.addEventListener("mozfullscreenchange",() => this.fullScreenIsClosing(), false);
+          document.addEventListener("webkitfullscreenchange",() => this.fullScreenIsClosing(), false);
+          document.addEventListener("msfullscreenchange",() => this.fullScreenIsClosing(), false);
+     }
+     
+     // Change fullscreen state when closing the fullscreen mode
+     fullScreenIsClosing() {
+          var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+          if(!fullscreenElement)
+            this.setState({isFullscreen: ! this.state.isFullscreen});
      }
      
      navNext() {
@@ -32,34 +59,60 @@ class Reader extends React.Component {
         this.setState({isBright: ! this.state.isBright});
      }
      
+     toggleFullscreen(){
+       var elem = document.getElementById("reader");
+       
+       if(!this.state.isFullscreen)
+       {
+          if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+          } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+          } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+          } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen();
+          }          
+        this.setState({isFullscreen: ! this.state.isFullscreen});          
+       }
+       else
+       {        
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+          } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+          } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+          }
+       }
+     }
+     
+     zoomIn(){
+       var elem = document.getElementById("readerImage");       
+       elem.style.height = 110 * elem.height / 100 + "px";
+     }
+     
+     zoomOut(){
+       var elem = document.getElementById("readerImage");       
+       elem.style.height = 90 * elem.height / 100 + "px";
+     }
+     
+     zoomOff(){
+       var elem = document.getElementById("readerImage");       
+       elem.style.height = "100%";
+     }
+     
      
   render() {
-    function fullscreen(){
-      function e(){
-        var doc=document.documentElement,
-        requestFullScreen=doc.requestFullScreen||doc.webkitRequestFullScreen||doc.mozRequestFullScreen||doc.msRequestFullscreen;
-        "undefined"!=typeof requestFullScreen&&requestFullScreen&&(requestFullScreen.call(doc),
-        document.getElementById("readerContent").height()<=window.screen.height&&document.getElementById("readerContent").height(window.screen.height))
-      }
-      function a(){
-        var e=document,
-        a=e.cancelFullScreen||e.webkitCancelFullScreen||e.mozCancelFullScreen||e.msExitFullscreen;
-        "undefined"!=typeof a&&a&&a.call(e)}
-        var i=navigator.userAgent.toLowerCase();
-        if(i.indexOf("msie")>-1||i.indexOf("rv:11")>-1)
-          return document.getElementById("fullScreen").hide(), $(".light").css("margin-right","21px"), !1;
-        var t=["fullScreen","fullScreen-back"], n=0;
-        document.getElementById("fullScreen").bind("click",function(){
-          return n=model.getIndexOfArr($(this).attr("class"),t),$(this).addClass(t[1-n]).removeClass(t[n]),0==n?e():a(),!1
-          })
-    }
-        var {title} = this.props;
+        var {title} = this.props; 
   return <div className="pageContent">
             <h4>Reader</h4>
             <center>
               <h5>One Piece</h5> 
               <div id="fadeReader" className="blackOverlay"></div>
-              <div className="pageContentCenter">     
+              <div className="pageContentCenter" id="reader">     
                 <div className="listFilters aboveFade">
                   <table>
                   <tbody>
@@ -84,15 +137,21 @@ class Reader extends React.Component {
                       </td>
                       <td>
                         <center>
-                          <a href="">
-                          <img alt="Zoom -" title="Zoom +" height="30px" width="30px" src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698840-icon-113-search-minus-128.png"/>
+                          <a href="javascript:void(0)" onClick={() => this.zoomOut()}>
+                          <img alt="Zoom -" title="Zoom -" height="30px" width="30px" src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698840-icon-113-search-minus-128.png"/>
                           </a>
-                          <a href="">
-                          <img alt="Zoom +" title="Zoom -" height="30px" width="30px" src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698628-icon-112-search-plus-128.png"/>
+                          <a href="javascript:void(0)" onClick={() => this.zoomIn()}>
+                          <img alt="Zoom +" title="Zoom +" height="30px" width="30px" src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698628-icon-112-search-plus-128.png"/>
+                          </a>
+                           <a href="javascript:void(0)" title="Zoom 100%" onClick={() => this.zoomOff()}>
+                             100%
                           </a>
                         </center>
                       </td>
                       <td>
+                       <a href="javascript:void(0)" onClick={() => this.toggleFullscreen()}>
+                          {this.state.isFullscreen ? "Windowed" : "Fullscreen"}                          
+                        </a>
                         <a href="javascript:void(0)" onClick={() => this.toggleFade()}>
                           {this.state.isBright ? "Turn dark" : "Turn bright"}
                         </a>
@@ -101,7 +160,7 @@ class Reader extends React.Component {
                   </tbody>
                 </table>
                 </div>  
-                <div className="readerContent aboveFade">
+                <div className="readerContent aboveFade" id="readerContent">
                   {this.state.previous == null ? <span></span> :<a href="javascript:void(0)" onClick={() => this.navPrevious()}>
                     <div id="readerRight"> 
                           <div className="readerNav" id="readerNavLeft">                        
@@ -109,8 +168,8 @@ class Reader extends React.Component {
                           </div>
                     </div>                
                   </a>}
-                  <center id="readerImage">
-                    <img src={this.state.current}/>
+                  <center>
+                    <img id="readerImage" src={this.state.current}/>
                   </center>
                   {this.state.next == null ? <span></span> : <a href="javascript:void(0)" onClick={() => this.navNext()}>
                     <div id="readerLeft">
