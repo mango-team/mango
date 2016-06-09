2afd60a4ad1142ef5eb832adb79388c6fdb2d105
Alexius Calamus <AlexiusCalamus@users.noreply.github.com>
2016-06-09 20:09:18 +0200

    Moved data to data folder

---
+++ client/src/data/Navigations.js
@@ -0,0 +1,34 @@
+const navList = [
+                  {
+                    name : "Home",
+                    isActive : true,
+                    isDisabled : false
+                  },
+                  {
+                    name : "Playlists",
+                    isActive : false,
+                    isDisabled : true
+                  },
+                  {
+                    name : "Subscriptions",
+                    isActive : false,
+                    isDisabled : false
+                  },
+                  {
+                    name : "History",
+                    isActive : false,
+                    isDisabled : false
+                  },
+                  {
+                    name : "Discussions",
+                    isActive : false,
+                    isDisabled : true
+                  },
+                  {
+                    name : "Settings",
+                    isActive : false,
+                    isDisabled : true
+                  }
+                ];
+                
+export default navList;
