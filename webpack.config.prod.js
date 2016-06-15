8855ad1e46aeb54b5836394248c089e033c09548
Alidou Ouedraogo <oalidou@gmail.com>
2016-06-16 00:23:21 +0200

    fix jsx module loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -40,10 +40,9 @@ module.exports = {
     loaders: [
     // js
     {
-        test: /(\.js|\.jsx)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: { presets: ['es2015', 'stage-0', 'react'] }
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'client')
     }, {
         test: /(\.scss|\.css)$/,
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
